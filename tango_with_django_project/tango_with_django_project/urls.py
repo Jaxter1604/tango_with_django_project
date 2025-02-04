289604d12f47cd669c2ee2b8cc568838ed7e1811
Jack Little <jtlittle@hotmail.co.uk>
2025-02-04 15:54:33 +0000

    all chapter 3 tests passed

---
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from rango import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('rango/', include('rango.urls')),
+    path('admin/', admin.site.urls),
+]
