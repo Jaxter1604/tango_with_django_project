6b666f6533e8f780c3b9fe779a3d439f375c33b6
Jack Little <jtlittle@hotmail.co.uk>
2025-02-06 08:49:44 +0000

    updated category and index page

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rango import views
 
+app_name = 'rango'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
